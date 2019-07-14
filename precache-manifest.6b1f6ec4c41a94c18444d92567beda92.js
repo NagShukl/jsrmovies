bbacd722e804a3ee1cbe714546e5195ec33571cc
Nagendra <nagendra.shukla@gmail.com>
2019-07-15 07:41:14 +1000

    Updates

---
+++ precache-manifest.6b1f6ec4c41a94c18444d92567beda92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6217442a0388a196dc191562128e22f",
+    "url": "/jsrmovies/index.html"
+  },
+  {
+    "revision": "7a85f24b73541948b031",
+    "url": "/jsrmovies/static/css/main.e1c750d4.chunk.css"
+  },
+  {
+    "revision": "f3dc7627325fe066cf9e",
+    "url": "/jsrmovies/static/js/2.ad3cb00c.chunk.js"
+  },
+  {
+    "revision": "7a85f24b73541948b031",
+    "url": "/jsrmovies/static/js/main.b8650d8c.chunk.js"
+  },
+  {
+    "revision": "c1122a6894395bf0cf6f",
+    "url": "/jsrmovies/static/js/runtime~main.2daa4e1b.js"
+  }
+]);
